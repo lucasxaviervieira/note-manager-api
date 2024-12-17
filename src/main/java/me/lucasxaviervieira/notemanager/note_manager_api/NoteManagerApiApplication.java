afb3a94ee4d70f5650eee3c31c50b9b95b1c05dc
lucasxaviervieira <lucasdavid.xavier@gmail.com>
2024-12-17 17:47:55 -0300

    Initial Commit

---
+++ src/main/java/me/lucasxaviervieira/notemanager/note_manager_api/NoteManagerApiApplication.java
@@ -0,0 +1,13 @@
+package me.lucasxaviervieira.notemanager.note_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/lucasxaviervieira/notemanager/note_manager_api/NoteManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lucasxaviervieira.notemanager.note_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
